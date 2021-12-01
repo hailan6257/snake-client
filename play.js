b83e7bc4f80a7c5dadb0c957e81660699cf53f7a
Hailan Wang <hailan6257@email.com>
2021-12-01 16:09:18 -0500

    Add setupInput and handleUserInput will soon use to control the snake.

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
-const {net, connect} = require('./client');//should require file name not function name
+const {connect} = require('./client');//should require file name not function name
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+const handleUserInput = () => {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') process.exit();
+  });
+};
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+setupInput();
+
