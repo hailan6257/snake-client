70755656bfc5e88733208dc40584cfb4845dfd8e
Hailan Wang <hailan6257@email.com>
2021-12-01 16:16:14 -0500

    Input code removed to input.js

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const {connect} = require('./client');//should require file name not function name
+const {setupInput} = require('./input');
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-const handleUserInput = () => {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') process.exit();
-  });
-};
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 setupInput();
 
