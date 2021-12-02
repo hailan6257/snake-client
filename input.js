e6c11cc644e1981de16159596c30b0b5fc090384
Hailan Wang <hailan6257@email.com>
2021-12-02 00:17:33 -0500

    Now snake can say hi

--- input.js
+++ input.js
@@ -1,10 +1,14 @@
 // Stores the active TCP connection object.
 let connection;
+const mes = ['Hi!Snakes', 'Well Done Snakes', 'Good Snakes'];
+const getRandomInt = max => Math.floor(Math.random() * max);
+const sayHi = mes => mes[getRandomInt(mes.length)];
 const handleUserInput = (key) => {
   //const moveK = m => setInterval(() => connection.write(m), 100);
   const moveT = (starter, move, ender) => {
     do {
       setTimeout(() => connection.write(move),starter);
+      setTimeout(() => connection.write(`Say: ${sayHi(mes)}`),starter);
       starter += 3;
     } while (starter < ender);
   };
@@ -13,6 +17,9 @@ const handleUserInput = (key) => {
   if (key === 'd') moveT(0, "Move: right", 30);
   if (key === 's') moveT(0, "Move: down", 30);
   if (key === '\u0003') process.exit();
+  // if (key) {
+  //   setTimeout(() => connection.write(`Say: ${sayHi(mes)}`), 2000);
+  // }
 };
 const setupInput = (conn) => {
   connection = conn;
